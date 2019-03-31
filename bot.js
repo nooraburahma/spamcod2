d8d78030df266a85770fb6e087015f240b9f325c
nooraburahma <47223988+nooraburahma@users.noreply.github.com>
2019-03-31 20:12:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("561611339731304448")
+let channel =     client.channels.get("561960865335869478")
 setInterval(function() {
 channel.send(`spam spam spam spam`);
 }, 30)
